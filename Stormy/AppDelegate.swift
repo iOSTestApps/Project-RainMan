43d7a3c3eeb76508ff66d5e3f26dc66910c62f01
buddybuild <help@buddybuild.com>
2015-09-15 12:39:25 -0700

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- Stormy/AppDelegate.swift
+++ Stormy/AppDelegate.swift
@@ -12,6 +12,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
    
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+       BuddyBuildSDK.setup()
+       
        
         application.setStatusBarHidden(true, withAnimation: .None)
         
